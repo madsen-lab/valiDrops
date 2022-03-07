67316666682032e509544af0a468b8d85b68c93e
Jesper Madsen <jespergrud@gmail.com>
2022-03-07 15:06:27 +0100

    Updated quality_metrics function
    No more dependency on mygene

--- R/quality_metrics.R
+++ R/quality_metrics.R
@@ -8,6 +8,12 @@
 #' @param mito A vector of the identifiers for mitochondrial genes or "auto". If set to "auto", valiDrops will automatically find the identifiers of mitochondrial genes [default = "auto"].
 #' @param ribo A vector of the identifiers for ribosomal genes or "auto". If set to "auto", valiDrops will automatically find the identifiers of ribosomal genes [default = "auto"].
 #' @param coding A vector of the identifiers for protein-coding genes or "auto". If set to "auto", valiDrops will automatically find the identifiers of protein-coding genes [default = "auto"].
+<<<<<<< Updated upstream
+=======
+#' @param species A character identifying the species of origin or "auto". See details for more information. If set to "auto", valiDrops will automatically determine the species if possible [default = "auto"].
+#' @param annotation A character identifying the gene annotation or "auto". See details for more information. If set to "auto", valiDrops will automatically determine the gene annotation if possible [default = "auto"].
+#' @param verbose A boolean (TRUE or FALSE) indicating whether or not to be verbose [default = TRUE].
+>>>>>>> Stashed changes
 #'
 #' @details
 #' \strong{Using a contrast matrix}\cr
@@ -16,12 +22,21 @@
 #'   \item \strong{Single-cell RNA-seq:} The main count matrix often contain only exon-derived reads. The contrast matrix should contains all reads (exons + intron) and the contrast_type should be "numerator".
 #'   \item \strong{Single-nucleus RNA-seq:} The main count matrix often contains all reads (exons + intron). The contrast matrix should only contains exon-derived reads and the contrast_type should be "denominator".
 #' }
+#'
+#' \strong{Available species}\cr
+#' Current valiDrops supports the following species: "human", "mouse", "rat", "C.elegans", "drosophila" and "zebrafish"
+#'
+#' \strong{Available gene annotations}\cr
+#' Current valiDrops supports the following gene annotations: "symbol", "ensembl", "entrez", "HGNC" and "MGI"
 #' @return A data frame object that contains the name of each barcode along with various quality metrics
 #' @export
 #' @import Matrix
-#' @import mygene
 
+<<<<<<< Updated upstream
 quality_metrics = function(counts, contrast = NULL, contrast_type = "denominator", mito = "auto", ribo = "auto", coding = "auto") {
+=======
+quality_metrics = function(counts, contrast = NULL, contrast_type = "denominator", species = "auto", annotation = "auto", mito = "auto", ribo = "auto", coding = "auto", verbose = TRUE) {
+>>>>>>> Stashed changes
   ## evaluate arguments
   # count matrix
   if(missing(counts)) {
@@ -29,7 +44,21 @@ quality_metrics = function(counts, contrast = NULL, contrast_type = "denominator
   } else {
     if (!any(class(counts) == c("dgTMatrix", "Matrix","matrix", "dgCMatrix"))) { stop('Count matrix has an unacceptable format. Accepted formats: matrix, Matrix, dgTMatrix, dgCMatrix', call. = FALSE) }
   }
+<<<<<<< Updated upstream
   
+=======
+
+  # species arugment
+  if (!any(species %in% c("Human","human","sapiens","Sapiens","h.sapiens", "H.Sapiens","H.sapiens","Mouse","mouse","Musculus","musculus","m.musculus","M.Musculus","M.musculus", "rat","Rat","Norvegicus","norvegicus","r.norvegicus","R.Norvegicus","R.norvegicus","worm","Worm","elegans","Elegans","C.Elegans","c.elegens","C.elegans","fly","Fly","drosophila","Drosophila","D.melanogaster","d.melanogaster","D.Melanogaster","zebrafish","Zebrafish","D.rerio","d.rerio","D.Rerio", "auto"))) {
+    stop('Species must be either "auto", "human", "mouse","rat", "C.elegans", "drosophila" or "zebrafish"', call. = FALSE)
+  }
+
+  # annotation arugment
+  if (!any(annotation %in% c("Symbol", "symbol","entrez","Entrez","NCBI","Ensembl","ensembl","HGNC","MGI", "auto"))) {
+    stop('annotation must be either "auto", "symbol", "ensembl" or "entrez"', call. = FALSE)
+  }
+
+>>>>>>> Stashed changes
   # contrast matrix
   if(!is.null(contrast)) {
     if (!any(class(contrast) == c("dgTMatrix", "Matrix","matrix", "dgCMatrix"))) { stop('Contrast matrix has an unacceptable format. Accepted formats: matrix, Matrix, dgTMatrix, dgCMatrix', call. = FALSE) }
@@ -40,24 +69,32 @@ quality_metrics = function(counts, contrast = NULL, contrast_type = "denominator
   
   # mito argument
   if (mito != "auto") {
-    if (sum(rownames(counts) %in% mito) != length(mito)) { stop('The count matrix does not contain all of the entered mitochondrial genes', call. = FALSE) }
+    if (sum(rownames(counts) %in% mito) != length(mito)) { stop('The count matrix does not contain all of the entered mitochondrial genes. Continueing', call. = TRUE) }
   }
   
   # ribo argument
   if (ribo != "auto") {
-    if (sum(rownames(counts) %in% ribo) != length(ribo)) { stop('The count matrix does not contain all of the entered ribosomal genes', call. = FALSE) }
+    if (sum(rownames(counts) %in% ribo) != length(ribo)) { stop('The count matrix does not contain all of the entered ribosomal genes. Continueing', call. = TRUE) }
   }
   
   # coding argument
   if (coding != "auto") {
-    if (sum(rownames(counts) %in% coding) != length(coding)) { stop('The count matrix does not contain all of the entered protein-coding genes', call. = FALSE) }
+    if (sum(rownames(counts) %in% coding) != length(coding)) { stop('The count matrix does not contain all of the entered protein-coding genes. Continueing', call. = TRUE) }
   }
+<<<<<<< Updated upstream
   
+=======
+
+  ## Check the verbose parameter
+  if (!isTRUE(verbose) & !isFALSE(verbose)) { stop("verbose must be either TRUE or FALSE") }
+
+>>>>>>> Stashed changes
   ## convert the counts into dgCMatrix if its class() is not dgCMatrix
   if(class(counts) != "dgCMatrix") { counts = as(counts, "dgCMatrix") }
   
   ## create a list for holding the output
   output <- list()
+<<<<<<< Updated upstream
   
   ## extract the names of all expressed genes
   genes <- data.frame(name = rownames(counts))
@@ -116,6 +153,119 @@ quality_metrics = function(counts, contrast = NULL, contrast_type = "denominator
     pcgenes <- coding
   }
   
+=======
+
+  ## Import datasets
+  data(human, package = "valiDrops")
+  data(mouse, package = "valiDrops")
+  data(rat, package = "valiDrops")
+  data(zebrafish, package = "valiDrops")
+  data(worm, package = "valiDrops")
+  data(fly, package = "valiDrops")
+
+  ## Create a list
+  ds <- list()
+  ds[[1]] <- as.data.frame(human)
+  ds[[2]] <- as.data.frame(mouse)
+  ds[[3]] <- as.data.frame(rat)
+  ds[[4]] <- as.data.frame(zebrafish)
+  ds[[5]] <- as.data.frame(worm)
+  ds[[6]] <- as.data.frame(fly)
+
+  ## Determine dataset
+  if (species == "auto") {
+    ds.range <- 1:6
+  } else if (any(species %in% c("Human","human","sapiens","Sapiens","h.sapiens", "H.Sapiens","H.sapiens"))) {
+    ds.range <- 1
+  } else if (any(species %in% c("Mouse","mouse","Musculus","musculus","m.musculus","M.Musculus","M.musculus"))) {
+    ds.range <- 2
+  } else if (any(species %in% c("rat","Rat","Norvegicus","norvegicus","r.norvegicus","R.Norvegicus","R.norvegicus"))) {
+    ds.range <- 3
+  } else if (any(species %in% c("zebrafish","Zebrafish","D.rerio","d.rerio","D.Rerio"))) {
+    ds.range <- 4
+  } else if (any(species %in% c("worm","Worm","elegans","Elegans","C.Elegans","c.elegens","C.elegans"))) {
+    ds.range <- 5
+  } else if (any(species %in% c("fly","Fly","drosophila","Drosophila","D.melanogaster","d.melanogaster","D.Melanogaster"))) {
+    ds.range <- 6
+  }
+
+  ## extract the names of all expressed genes
+  genes <- data.frame(name = rownames(counts))
+  genes$clean <- genes$name
+
+  ## strip dots if the input annotation is ensembl (dots to use versions of Ensembl genes is used by GENCODE)
+  ens.idx <- grep("^ENS", genes$name)
+  if (length(ens.idx) > 0) {
+    ens.id <- genes[ ens.idx, "name"]
+    lengths <- data.frame(dot = as.numeric(regexpr("\\.", ens.id))-1, total = nchar(as.character(ens.id)))
+    lengths$use <- apply(lengths, 1, FUN = function(x) min(x[x > 0]))
+    ens.clean <- substr(genes$name, 0, lengths$use)
+    genes[ ens.idx, "clean"] <- ens.clean
+  }
+
+  ## Loop across selected datasets
+  ann.stats <- as.data.frame(matrix(ncol=3, nrow=length(ds.range)))
+  counter <- 1
+  for (ds.sel in ds.range) {
+    tmp <- ds[[ds.sel]]
+    if (annotation == "auto") {
+      for (col in 1:ncol(tmp)) {
+        ann.stats[counter,1] <- ds.sel
+        ann.stats[counter,2] <- col
+        ann.stats[counter,3] <- sum(genes$clean %in% tmp[,col])
+        counter <- counter + 1
+      }
+    } else {
+      if (annotation %in% c("Symbol", "symbol")) { col <- which(colnames(tmp) == "Symbol") }
+      if (annotation %in% c("entrez", "Entrez", "NCBI")) { col <- which(colnames(tmp) == "NCBI") }
+      if (annotation %in% c("Ensembl", "ensembl")) { col <- which(colnames(tmp) == "Ensembl") }
+      if (annotation %in% c("HGNC")) { col <- which(colnames(tmp) == "HGNC") }
+      if (annotation %in% c("MGI")) { col <- which(colnames(tmp) == "MGI") }
+      ann.stats[counter,1] <- ds.sel
+      ann.stats[counter,2] <- col
+      ann.stats[counter,3] <- sum(genes$clean %in% tmp[,col])
+    }
+  }
+
+  ## Get the best hits
+  best.ds <- ann.stats[ which.max(ann.stats[,3]),1]
+  best.col <- ann.stats[ which.max(ann.stats[,3]),2]
+  best.sum <- ann.stats[ which.max(ann.stats[,3]),3]
+
+  ## Protein-coding genes
+  if (coding == "auto") {
+    pcgenes <- ds[[best.ds]][ ds[[best.ds]]$Type %in% "protein_coding",best.col]
+    pcgenes <- genes[ genes$clean %in% pcgenes,1]
+  } else {
+    pcgenes <- coding
+  }
+
+  ## Mitochondrial genes
+  if (mito == "auto") {
+    mitogenes <- ds[[best.ds]][ ds[[best.ds]]$Chr %in% c("MtDNA","MT","mitochondrion_genome"),best.col]
+    mitogenes <- genes[ genes$clean %in% mitogenes,1]
+  } else {
+    mitogenes <- mito
+  }
+
+  ## Ribosomal genes
+  if (ribo == "auto") {
+    ribogenes <- ds[[best.ds]][ c(grep("^rpl", tolower(ds[[best.ds]]$Symbol)),grep("^rps", tolower(ds[[best.ds]]$Symbol))),best.col]
+    ribogenes <- genes[ genes$clean %in% ribogenes,1]
+  } else {
+    ribogenes <- ribo
+  }
+
+  ## Tell the user about the match
+  if (verbose) {
+    species <-  c("Human", "Mouse","Rat","Zebrafish","Worm","Fly")[best.ds]
+    annotation <- colnames(ds[[best.ds]])[best.col]
+    if (annotation == "NCBI") { annotation <- "Entrez" }
+    message(paste("Detected sample origin: ", species, ". Detected gene annotation: ", annotation, ". Mapped ", best.sum, "/", nrow(genes), " (", signif((best.sum/nrow(genes))*100,3), "%) of input IDs.", sep=""))
+    message(paste("Found ", length(mitogenes), " mitochondrial genes, ", length(ribogenes), " ribosomal genes, and ", length(pcgenes), " protein-coding genes.", sep=""))
+  }
+
+>>>>>>> Stashed changes
   ## calculate quality metrics
   metrics <- as.data.frame(matrix(ncol=6, nrow=ncol(counts)))
   colnames(metrics) <- c("barcode","logUMIs","logFeatures","mitochondrial_fraction","ribosomal_fraction","coding_fraction")
---
+++ R/species_data.R
@@ -0,0 +1,116 @@
+#' A dataset containing human genes annotations
+#'
+#' @format A data frame with 106851 rows and 7 variables:
+#' \describe{
+#' \item{NCBI}{NCBI gene (formerly Entrezgene) ID}
+#' \item{HGNC}{HGNC ID}
+#' \item{Ensembl}{Stable Ensemble gene ID}
+#' \item{Chr}{Chromosome scaffold name}
+#' \item{Symbol}{Official gene name}
+#' \item{Type}{Gene type}
+#' \item{Alias}{Gene synonym/aliases}
+#' }
+#'
+#' @source \url{http://www.ensembl.org/}
+#' @docType data
+#' @keywords datasets
+#' @name human
+#' @usage data(human)
+NULL
+
+#' A dataset containing mouse genes annotations
+#'
+#' @format A data frame with 82003 rows and 7 variables:
+#' \describe{
+#' \item{NCBI}{NCBI gene (formerly Entrezgene) ID}
+#' \item{MGI}{MGI ID}
+#' \item{Ensembl}{Stable Ensemble gene ID}
+#' \item{Chr}{Chromosome scaffold name}
+#' \item{Symbol}{Official gene name}
+#' \item{Type}{Gene type}
+#' \item{Alias}{Gene synonym/aliases}
+#' }
+#'
+#' @source \url{http://www.ensembl.org/}
+#' @docType data
+#' @keywords datasets
+#' @name mouse
+#' @usage data(mouse)
+NULL
+
+#' A dataset containing C.elegans genes annotations
+#'
+#' @format A data frame with 46912 rows and 6 variables:
+#' \describe{
+#' \item{NCBI}{NCBI gene (formerly Entrezgene) ID}
+#' \item{Ensembl}{Stable Ensemble gene ID}
+#' \item{Chr}{Chromosome scaffold name}
+#' \item{Symbol}{Official gene name}
+#' \item{Type}{Gene type}
+#' \item{Alias}{Gene synonym/aliases}
+#' }
+#'
+#' @source \url{http://www.ensembl.org/}
+#' @docType data
+#' @keywords datasets
+#' @name worm
+#' @usage data(worm)
+NULL
+
+#' A dataset containing Drosophila melanogaster genes annotations
+#'
+#' @format A data frame with 77216 rows and 6 variables:
+#' \describe{
+#' \item{NCBI}{NCBI gene (formerly Entrezgene) ID}
+#' \item{Ensembl}{Stable Ensemble gene ID}
+#' \item{Chr}{Chromosome scaffold name}
+#' \item{Symbol}{Official gene name}
+#' \item{Type}{Gene type}
+#' \item{Alias}{Gene synonym/aliases}
+#' }
+#'
+#' @source \url{http://www.ensembl.org/}
+#' @docType data
+#' @keywords datasets
+#' @name fly
+#' @usage data(fly)
+NULL
+
+#' A dataset containing rat genes annotations
+#'
+#' @format A data frame with 47954 rows and 6 variables:
+#' \describe{
+#' \item{NCBI}{NCBI gene (formerly Entrezgene) ID}
+#' \item{Ensembl}{Stable Ensemble gene ID}
+#' \item{Chr}{Chromosome scaffold name}
+#' \item{Symbol}{Official gene name}
+#' \item{Type}{Gene type}
+#' \item{Alias}{Gene synonym/aliases}
+#' }
+#'
+#' @source \url{http://www.ensembl.org/}
+#' @docType data
+#' @keywords datasets
+#' @name rat
+#' @usage data(rat)
+NULL
+
+#' A dataset containing zebrafish genes annotations
+#'
+#' @format A data frame with 67809 rows and 6 variables:
+#' \describe{
+#' \item{NCBI}{NCBI gene (formerly Entrezgene) ID}
+#' \item{Ensembl}{Stable Ensemble gene ID}
+#' \item{Chr}{Chromosome scaffold name}
+#' \item{Symbol}{Official gene name}
+#' \item{Type}{Gene type}
+#' \item{Alias}{Gene synonym/aliases}
+#' }
+#'
+#' @source \url{http://www.ensembl.org/}
+#' @docType data
+#' @keywords datasets
+#' @name zebrafish
+#' @usage data(zebrafish)
+NULL
+
