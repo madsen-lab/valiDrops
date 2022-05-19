7fea984aad182a563b096f752a1bc9d80db6e40d
Gabija Kavaliauskaitė <57943176+gabija-kavaliauskaite@users.noreply.github.com>
2022-05-19 11:26:45 +0200

    Updated label_apoptotic

--- R/label_apoptotic.R
+++ R/label_apoptotic.R
@@ -4,11 +4,10 @@
 #'
 #' @param counts A matrix containing counts for barcodes passing the barcode-rank threshold. See \link{rank_barcodes}
 #' @param metrics A data frame containing quality metrics. See \link{quality_metrics} and \link{quality_filter}
-#' @param zscore A number indicating the Z score to use for soft labeling of apoptotic cells [default = 3].
+#' @param mitochondrial.threshold A mitochondrial threshold identified using quality_filter method. See \link{quality_filter}
+#' @param threshold A number indicating the score to use for soft labeling of apoptotic cells [default = 0.2].
 #' @param nfeats A number indicating the number of variable features to use for clustering [default = 5000].
 #' @param npcs A number indicating the number of singular values to use for clustering [default = 100].
-#' @param anomaly.quantile A number indicating the quantile to use for detecting anomalous barcodes [default = 0.1].
-#' @param plot A boolean (TRUE or FALSE) indicating whether or not to produce plots [default = TRUE].
 #' @param seed Set a seed for reproducible results. Set to NULL to run in non-deterministic mode [default = 42].
 #' @param verbose Set to TRUE for verbose mode. Set to FALSE for silent mode [default = TRUE].
 #'
@@ -18,30 +17,28 @@
 #' @import mixtools 
 #' @import solitude
 #' @import scry
+#' @import classInt
 #' @importFrom Seurat GetAssayData
 #' @importFrom SingleCellExperiment counts
 #' @importFrom sparseMatrixStats colSds
-
-label_apoptotic = function(counts, metrics, zscore = 3, nfeats = 5000, npcs = 20, anomaly.quantile = 0.15, plot = TRUE, seed = 42, verbose = FALSE) {
-  
-  ## Check the plot parameter
-  if (!isTRUE(plot) & !isFALSE(plot)) { stop("plot must be either TRUE or FALSE") }
+#' 
+label_apoptotic = function(counts, metrics, mitochondrial.threshold, threshold = 0.2, nfeats = 5000, npcs = 20, seed = 42, verbose = FALSE){
   
   ## Check the verbose parameter
   if (!isTRUE(verbose) & !isFALSE(verbose)) { stop("verbose must be either TRUE or FALSE") }
   
-  ## Check the anomaly.quantile parameter
-  if (anomaly.quantile < 0 | anomaly.quantile > 1) { stop("anomaly.quantile must be larger than 0 and less than 1") }
+  ## Check the threshold parameter
+  if (mitochondrial.threshold <= 0) { stop("zscore must be larger than 0") }
   
-  ## Check the zscore parameter
-  if (zscore <= 0) { stop("zscore must be larger than 0") }
+  ## Check the threshold parameter
+  if (threshold <= 0) { stop("zscore must be larger than 0") }
   
   # nfeats argument
   if(class(nfeats) != "numeric" | nfeats <= 0 | nfeats < npcs) stop('nfeats needs to be a numeric greater than 0 and than npcs', call. = FALSE)
   
   # npcs argument
   if(class(npcs) != "numeric" | npcs <= 0) stop('npcs needs to be a numeric greater than 0', call. = FALSE)
-
+  
   ## Extract counts from Seurat or SCE objects
   if (any(class(counts) %in% c("SingleCellExperiment", "Seurat"))) {
     if (class(counts) == "SingleCellExperiment") {
@@ -74,22 +71,21 @@ label_apoptotic = function(counts, metrics, zscore = 3, nfeats = 5000, npcs = 20
   if (verbose) { message("Data formats and parameters succesfully checked. Starting the apoptotic cell run.\n") }
   start = Sys.time()
   
-  #uses quality_counts.metrics to identify anomalies based on the z-score for soft labeling 
-  metrics$fraction = (1-metrics$ribosomal_fraction) + (1-metrics$coding_fraction) + metrics$mitochondrial_fraction
-  metrics$fraction_Z = (metrics$fraction - mean(metrics$fraction)) / sd(metrics$fraction)
-  metrics$s <- 0
-  metrics[ metrics$fraction_Z > zscore, "s"] <- 1
- 
-  ##Return a plot showing the fraction of z scores and selected threshold 
-  if(plot){
-    plot(metrics$fraction_Z, col = ifelse(metrics$fraction_Z > zscore,"red", "black"), pch = 16, ylab = "Z score", las = 1)
-    abline(h = zscore, col = "black", lwd = 2, lty=2)
-  }
+  metrics <- metrics[metrics$mitochondrial_fraction <= mitochondrial.threshold,]
   
-  ####### TODO: Check that *enough* barcodes pass the threshold, else stop
-
-  ## Extract feature sets
-  counts = counts[ , colnames(counts) %in% rownames(metrics)]
+  # Transformation the fractions
+  metrics$ribosomal_fraction <- asin(sqrt((metrics$ribosomal_fraction)))/(pi/2)
+  metrics$coding_fraction <- asin(sqrt((metrics$coding_fraction)))/(pi/2)
+  metrics$mitochondrial_fraction <- asin(sqrt(metrics$mitochondrial_fraction))/(pi/2)
+  
+  # Calculate a score
+  metrics$score <- metrics$mitochondrial_fraction * 6.1 - metrics$mitochondrial_fraction * metrics$ribosomal_fraction * 37 - metrics$mitochondrial_fraction * metrics$coding_fraction * 5 + metrics$mitochondrial_fraction * metrics$coding_fraction * metrics$ribosomal_fraction * 37
+  
+  # Threshold to get initial labels
+  metrics$s <- 0
+  metrics[metrics$score > threshold, "s"] <- 1
+  
+  counts = counts[ , colnames(counts) %in% metrics$barcode]
   nonzero = counts[ Matrix::rowSums(counts) > 0,]
   
   # Calculate size factors
@@ -119,29 +115,55 @@ label_apoptotic = function(counts, metrics, zscore = 3, nfeats = 5000, npcs = 20
   colnames(svd_data) = c("s", paste("SVD_", seq(1,npcs,1), sep=""))
   rownames(svd_data) = rownames(data.scaled)
   
-  # Define groups for isolationForest
+  #subsetting for Isolation Forest
   iF_dead = subset(svd_data, s == 1)
   iF_dead = iF_dead[,-1]
+  
   iF_healthy = subset(svd_data, s == 0)
   iF_healthy = iF_healthy[,-1]
-  index = sample(ceiling(nrow(iF_dead) * 0.9))
   
-  # Positive labeling
-  iforest = isolationForest$new(sample_size = length(index), num_trees = 200)
+  index = sample(ceiling(nrow(iF_dead) * 0.8))
+  
+  #Positive labeling
+  iforest = isolationForest$new(sample_size = length(index),
+                                num_trees = 500)
   iforest$fit(iF_dead)
   
   # Calculate anomaly scores for 'healthy' labels
   scores_unlabeled <- as.data.frame(iforest$predict(data = iF_healthy))
   scores_unlabeled <- scores_unlabeled[ order(scores_unlabeled$anomaly_score, decreasing = TRUE),]
-  quantile_cut <- quantile(scores_unlabeled$anomaly_score, anomaly.quantile)
+  
+  #Identify the breaks on the anomaly scores
+  intervals = classIntervals(scores_unlabeled$anomaly_score, style = "quantile", thr = 0)
+  intervals = intervals$brks[3]
+  
+  #Subset the data based on the biggest jump
+  midpoint = subset(scores_unlabeled, anomaly_score < intervals)
+  
+  #break into intervals and define 
+  decision_boundary = classIntervals(midpoint$anomaly_score, style = "quantile", thr = 0)
+  decision_boundary = unique(decision_boundary$brks)
+  
+  #Identifying plateau point
+  plateau_point <- c(decision_boundary[5:length(decision_boundary)], rep(NA, 4)) - decision_boundary
+  plateua_index <- decision_boundary %in% decision_boundary[which.min(plateau_point):(which.min(plateau_point)+4)]
 
-  ####### POTENTIAL IMPROVEMENT: Create better function to detect the breakpoint in anomaly scores, instead of using a quantile-based cutoff
-
+  #Final cut-off
+  decision_boundary = decision_boundary[plateua_index == TRUE][1]
+  
   # Re-label based on the anomaly scores
-  svd_data[ rownames(svd_data) %in% rownames(iF_healthy)[ scores_unlabeled$anomaly_score < quantile_cut],"s"] <- 1
+  svd_data[ rownames(svd_data) %in% rownames(iF_healthy)[ scores_unlabeled$anomaly_score <= decision_boundary],"s"] <- 1
+  svd_data$barcode <- rownames(svd_data)
   
-  # Return
-  return(rownames(svd_data[ svd_data$s == 1,]))
- }
+  #Select barcodes that were marked as apoptotic
+  apoptotic <- svd_data[ svd_data$s == 1, "barcode"]
   
- 
+  #return apoptotic cells
+  return(apoptotic)
+}
+
+
+
+
+
+
