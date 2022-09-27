0372013ca026366886e4384f3099867c0c6a222e
Jesper Madsen <jespergrud@gmail.com>
2022-09-27 14:04:47 +0200

    Update valiDrops.R

--- R/valiDrops.R
+++ R/valiDrops.R
@@ -83,10 +83,9 @@ valiDrops = function(counts, rank_barcodes = TRUE, mitochondrial_clusters = 3, r
 
   ## Run label_apoptotic
   if (label_apoptotic) {
-	rownames(metrics$metrics) <- metrics$metrics$barcode
-    apoptotic <- R.utils::doCall(valiDrops::label_apoptotic, args = ..., alwaysArgs = list(counts = counts.subset, metrics = metrics$metrics, mitochondrial.threshold = qc.pass$mitochondrial.threshold))
-	met$apoptotic <- "healthy"
-	met[ met$barcode %in% apoptotic, "apoptotic"] <- "apoptotic"
+     apoptotic <- R.utils::doCall(valiDrops::label_apoptotic, args = ..., alwaysArgs = list(counts = counts, metrics = met, qc.labels = setNames(as.character(met$qc), met$barcode)))	  
+     met$apoptotic <- "healthy"
+     met[ met$barcode %in% apoptotic, "apoptotic"] <- "apoptotic"
   }
 
   ## Return
